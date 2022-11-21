82303a722f298e12f1bb67c76c22c2497a1c3a86
mahad <mahad@DESKTOP-741VUHT>
2022-11-21 17:48:58 +0530

    !!

---
+++ in.jdbc.connection/src/in/jdbc/connection1/FirstJDBC.java
@@ -0,0 +1,23 @@
+package in.jdbc.connection1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class FirstJDBC {
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/youtube", "root", "");
+
+			if (con.isClosed()) {
+				System.out.println("connection is closed");
+			} else {
+				System.out.println("connection created...!!!!!!!!!!");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
